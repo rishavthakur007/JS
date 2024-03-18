1d6a8af6dabe9a5417fd4e6fd210cd34f9c0db86
rishavthakur007 <81691639+rishavthakur007@users.noreply.github.com>
2024-03-18 07:24:32 +0000

    conversion of DT

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,40 @@
+// let score = "33";
+
+// console.log(typeof score);
+// let val = Number(score);
+// console.log(typeof val);
+
+
+// let score = "33abc";
+
+// console.log(typeof score);
+// let val = Number(score);
+// console.log(typeof val);
+// console.log(val);
+
+
+// let score = null;
+
+// console.log(typeof score);
+// let val = Number(score);
+// console.log(typeof val);
+// console.log(val);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = null
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
