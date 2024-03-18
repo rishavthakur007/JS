76f46d3bea23f87af4cf4e5ce073d4abb9d8506a
rishavthakur007 <81691639+rishavthakur007@users.noreply.github.com>
2024-03-18 07:00:40 +0000

    variables and dataTypes commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234;
+let accountEmail = "rishav@gmail.com";
+var accountpassword = "77888";
+accountCity = "patna";
+accountEmail="thakur@gmail.com";
+accountpassword = "34444";
+accountCity = "delhi";
+console.table([accountId , accountEmail , accountpassword, accountCity]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
